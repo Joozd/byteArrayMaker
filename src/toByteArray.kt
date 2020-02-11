3e225555d80fb6b19d33ebdec85ea98e66a97e46
Joozd <joozd@ius.org>
2020-02-11 11:00:41 +0100

    Added support for encoding Pairs, decoding pair<Long,Long>

--- src/toByteArray.kt
+++ src/toByteArray.kt
@@ -1,7 +1,7 @@
 /**
  *  toByteArray extensions (and back)
  *  Changes primitive types to arrays of bytes with the same values
- *  version 1
+ *  version 2
  *  Copyright (c) 2020 Joost Welle
  *  You may redistribute/use/copy/modify/etc in whatever way you see fit.
  *  This file is "as is".
@@ -89,5 +89,47 @@ fun Char.toByteArray(): ByteArray{
  * Makes an Int from an Int.SIZE_BYTES byte ByteArray
  */
 fun charFromBytes(bytes: ByteArray): Char{
+    require(bytes.size == Char.SIZE_BYTES)
     return intFromBytes(ByteArray(Int.SIZE_BYTES - Char.SIZE_BYTES) + bytes).toChar()
 }
+
+/**
+ * returns an array of Int.SIZE_BYTES (==8) bytes, containing the same bits
+ */
+fun Short.toByteArray(): ByteArray{
+    return this.toInt().toByteArray().takeLast(Short.SIZE_BYTES).toByteArray()
+}
+/**
+ * Makes an Int from an Int.SIZE_BYTES byte ByteArray
+ */
+fun shortFromBytes(bytes: ByteArray): Short{
+    require(bytes.size == Short.SIZE_BYTES)
+    return intFromBytes(ByteArray(Int.SIZE_BYTES - Short.SIZE_BYTES) + bytes).toShort()
+}
+
+fun <T, R>  Pair<T, R>.toByteArray(): ByteArray{
+    val first = when (this.first){
+        is Long     -> (this.first as Long).toByteArray()
+        is Int      -> (this.first as Int).toByteArray()
+        is Char     -> (this.first as Char).toByteArray()
+        is Double   -> (this.first as Double).toByteArray()
+        is Float    -> (this.first as Float).toByteArray()
+        else        -> error("Unsupported type for ${this.first}")
+    }
+    val second = when (this.second){
+        is Long     -> (this.second as Long).toByteArray()
+        is Int      -> (this.second as Int).toByteArray()
+        is Char     -> (this.second as Char).toByteArray()
+        is Double   -> (this.second as Double).toByteArray()
+        is Float    -> (this.second as Float).toByteArray()
+        else        -> error("Unsupported type for ${this.second}")
+    }
+    return first + second
+}
+
+//probably not gonna make this for every possibility
+fun pairLongLongFromBytes(bytes:ByteArray): Pair<Long, Long>{
+    require(bytes.size == Long.SIZE_BYTES + Long.SIZE_BYTES)
+    return longFromBytes(bytes.slice(0 until Long.SIZE_BYTES).toByteArray()) to longFromBytes(bytes.slice(Long.SIZE_BYTES until bytes.size).toByteArray())
+}
+
